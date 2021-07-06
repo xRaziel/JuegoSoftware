ee6ac0179c64c0afbcb5579f5fa1348c4444dd7b
xRaziel <73146905+xRaziel@users.noreply.github.com>
2021-07-05 20:47:36 -0400

    Jueguito
    Primer avance

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    public float speed = 0.02f;
+    public float dirX;
+    public float dirY;
+
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirY = Input.GetAxis("Vertical");
+        transform.position += new Vector3(dirX * speed, dirY * speed, 0);
+    }
+
+}
---
+++ Assets/Scripts/Mochila.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mochila : MonoBehaviour
+{
+    public List<GameObject> Bag = new List<GameObject>();
+    public GameObject inv;
+    public bool activarInv;
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("Item") || coll.CompareTag("Cell"))
+        {
+            
+            for (int i = 0; i < Bag.Count; i++)
+            {
+                if (Bag[i].GetComponent<Image>().enabled == false)
+                {
+                    Debug.Log("Esta pasando");
+                    Bag[i].GetComponent<Image>().enabled = true;
+                    Bag[i].GetComponent<Image>().sprite = coll.GetComponent<SpriteRenderer>().sprite;
+                    coll.gameObject.SetActive(false);
+                    break;
+
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activarInv)
+        {
+            inv.SetActive(true);
+        }
+        else
+        {
+            inv.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activarInv = !activarInv;
+        }
+    }
+}
